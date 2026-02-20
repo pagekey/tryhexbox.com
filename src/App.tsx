61e2bed892093b4ca9abd3f02bf44382d58db985
Stephen Grice <stephengrice@live.com>
2026-02-19 21:46:31 -0500

    feat: add all components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import HowItWorks from "./components/HowItWorks"
+import Waitlist from "./components/Waitlist"
+
 function App() {
-  return <div className="bg-red-400e">Hello World!</div>
+  return <>
+    <Hero />
+    <Features />
+    <HowItWorks />
+    <Waitlist />
+    <Footer />
+  </>
 }
 
 export default App
